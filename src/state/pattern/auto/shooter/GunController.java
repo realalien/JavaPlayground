e2a8ffd0748e06a47c3f3bd9bbb195302724f3b3
Zhu Jia Cheng <realalien@gmail.com>
2009-07-30 18:14:24 +0800

    missing check in.

--- src/state/pattern/auto/shooter/GunController.java
+++ src/state/pattern/auto/shooter/GunController.java
@@ -1,14 +1,39 @@
 package state.pattern.auto.shooter;
 
-
 /**
  * 
- * a imitate the 'Miner' in "Programming AI by Example"
+ * To imitate the 'Miner' in "Programming AI by Example"
  * 
- * * I can't decide who uses GunStatus by passing-in as parameters of message call,
- *   so treat the controller like something with a brain. 
+ * * I can't decide who uses GunStatus by passing-in as parameters of message
+ * call, so treat the controller like something with a brain.
  * */
-public class GunController { //TODO: extends Weapon<common properties> implements <standards of a weapon> 
+public class GunController extends BaseGameEntity{ 
+	// TODO: extends Weapon<common properties>
+	//       implements <standards of a weapon>
 
+	public GunState presentGunState;
 	
+	//TODO: other properties
+
+	public void changeGunState(GunState newGunState) {
+		assert (presentGunState != null && newGunState != null);
+		
+		presentGunState.exit(this);
+		
+		presentGunState = newGunState;
+		
+		newGunState.enter(this);
+	}
+
+	@Override
+	public void update() {
+		// shall I clean and re-calibrate here? 
+		
+		if (presentGunState != null){
+			presentGunState.execute(this);
+		}
+	}
 }
+
+
+//AFM: deep study of 'State Design pattern'
--- src/state/pattern/auto/shooter/GunState.java
+++ src/state/pattern/auto/shooter/GunState.java
@@ -3,12 +3,12 @@ package state.pattern.auto.shooter;
 public interface GunState {
 
 	//this will execute when the state is entered
-	public void enter(GunController gc);
+	public void enter(GunController gs);
 
     //this is called by the miner's update function each update step
-	public void execute(GunController gc);
+	public void execute(GunController gs);
 
 	//this will execute when the state is exited
-	public void exit(GunController gc);
+	public void exit(GunController gs);
 
 }
